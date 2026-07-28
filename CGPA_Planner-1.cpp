ec7dd7b0522d558b57bb1d1df880759f8766d2b8
jawdali32 <jawadalimemonwd@gmail.com>
2026-07-28 22:00:13 +0500

    Implement CGPA Management and Prediction System

---
+++ CGPA_Planner-1.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int completedSemesters;
+
+    cout << "==============================\n";
+    cout << "CGPA MANAGEMENT SYSTEM\n";
+    cout << "==============================\n\n";
+
+    cout << "Enter number of completed semesters: ";
+    cin >> completedSemesters;
+
+    double totalGradePoints = 0;
+    double totalCredits = 0;
+
+    double semesterGPA[20];
+
+    for (int i = 0; i < completedSemesters; i++)
+    {
+        int courses;
+        cout << "\nSemester " << i + 1 << endl;
+        cout << "Enter number of courses: ";
+        cin >> courses;
+
+        double semGradePoints = 0;
+        double semCredits = 0;
+
+        for (int j = 0; j < courses; j++)
+        {
+            double creditHours, gradePoint;
+
+            cout << "\nCourse " << j + 1 << endl;
+            cout << "Credit Hours: ";
+            cin >> creditHours;
+
+            cout << "Grade Point (0 - 4): ";
+            cin >> gradePoint;
+
+            semGradePoints += gradePoint * creditHours;
+            semCredits += creditHours;
+        }
+
+        semesterGPA[i] = semGradePoints / semCredits;
+
+        cout << fixed << setprecision(2);
+        cout << "Semester GPA = " << semesterGPA[i] << endl;
+
+        totalGradePoints += semGradePoints;
+        totalCredits += semCredits;
+    }
+
+    double cgpa = totalGradePoints / totalCredits;
+
+    cout << "\n==================================";
+    cout << "\nSemester GPA Report";
+    cout << "\n==================================";
+
+    for (int i = 0; i < completedSemesters; i++)
+    {
+        cout << "\nSemester " << i + 1 << " GPA = "
+             << fixed << setprecision(2)
+             << semesterGPA[i];
+    }
+
+    cout << "\n\nCurrent CGPA = "
+         << fixed << setprecision(2)
+         << cgpa << endl;
+
+    // Prediction Feature
+
+    cout << "\n========== CGPA Prediction ==========\n";
+
+    int remainingSemesters;
+    cout << "Enter remaining semesters: ";
+    cin >> remainingSemesters;
+
+    double targetCGPA;
+    cout << "Enter target CGPA: ";
+    cin >> targetCGPA;
+
+    double futureCreditsPerSemester;
+    cout << "Expected credit hours per remaining semester: ";
+    cin >> futureCreditsPerSemester;
+
+    double futureCredits = remainingSemesters * futureCreditsPerSemester;
+
+    double totalFinalCredits = totalCredits + futureCredits;
+
+    double requiredGradePoints =
+        (targetCGPA * totalFinalCredits) - totalGradePoints;
+
+    double requiredGPA = requiredGradePoints / futureCredits;
+
+    cout << fixed << setprecision(2);
+
+    if (requiredGPA > 4.0)
+    {
+        cout << "\nTarget CGPA cannot be achieved.\n";
+        cout << "Required GPA = " << requiredGPA
+             << " (Greater than 4.00)\n";
+    }
+    else if (requiredGPA < 0)
+    {
+        cout << "\nCongratulations!\n";
+        cout << "You have already achieved the target CGPA.\n";
+    }
+    else
+    {
+        cout << "\nTo achieve a CGPA of "
+             << targetCGPA
+             << ", you need an average GPA of "
+             << requiredGPA
+             << " in the remaining semesters.\n";
+    }
+
+    cout << "\nThank You!\n";
+
+    return 0;
+}
